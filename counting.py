08a5a88ccae5d9545f75a7c46b9553c1ac5ea64b
giselaortt <giselaortt@usp.br>
2017-11-27 15:32:25 -0200

    sad

--- counting.py
+++ counting.py
@@ -1,11 +1,18 @@
 #import pandas as pd
 import numpy as np
+from sklearn.neural_network import MLPClassifier
 
 
-def counting( train_dataset, dataset, window_size, model  ):
-	model.fit(train_dataset)
+'''
+	model: should come trained
+	dataset: numpy objct. shouldnt contain the classes.
+'''
+def counting( dataset, window_size, model ):
+	x = []
+	y = []
 	for i in range( dataset.shape[0] - window_size ) :
-		
+		model.predict( dataset[i:i+window_size] )
+		y.append(  )
 
 	return
 
@@ -14,7 +21,7 @@ def main():
 	data = np.loadtxt( "Ic/Archive/data_44100_1024_treino.csv", skiprows = 1, delimiter = ',')
 	test = np.loadtxt("Ic/Archive/data_44100_1024_teste.csv", skiprows = 1, delimiter = ',' )
 	data = data[data[:,1].argsort()]
-	test = test[data[:,1].argsort()]
-
+	
+	
 
 main()
