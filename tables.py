7ea1d68e757a795a9ce5b1b549f250fda46bf38c
giselaortt <gisela.ortt@gmail.com>
2018-03-01 22:01:04 -0300

    mudando par ao paradigma de orientacao a objetos, para maior organizacao

--- tables.py
+++ tables.py
@@ -5,41 +5,50 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score
 
+
 '''
-imprime as tabelas de acertos e erros para um modelo. Por enquanto, implementado apenas para 2 classes.
-o modelo é treinado na propria função e as classes devem vir como 1 e 2.
-
-model: deve implementar as funções fit e predict
-name1: nome da classe 1
-name2: nome da classe 2
+	imprime as tabelas de acertos e erros para um modelo. Por enquanto, implementado apenas para 2 classes.
+	o modelo é treinado na propria função e as classes devem vir como 1 e 2.
+	model: deve implementar as funções fit e predict
+	self.name1: nome da classe 1
+	self.name2: nome da classe 2
+	only two classes by now
 '''
-def tables( train_x, test_x, train_y, test_y, model, name1, name2 ):
-	model.fit( train_x, train_y.flatten() )
-	out = [ round(model.predict( elem.reshape(1, -1) )) for elem in test_x ]
-	print 'total correct predicted', accuracy_score( out, test_y, normalize = False) 
-	print 'sample size', train_x.shape[0] , "\n" 
-	correct = [_==__ for _,__ in zip( out,test_y )]
 
-	one_correct =  np.sum( [ _ == True and cl == 1 for _, cl in zip(correct,  test_y) ] ) 
-	two_correct =  np.sum( [ _ == True and cl == 2 for _, cl in zip(correct,  test_y) ] )
-	one_false = np.sum( [ _ == False and cl == 1 for _, cl in zip(correct,  test_y) ] )
-	two_false = np.sum( [ _ == False and cl ==2 for _, cl in zip(correct,  test_y) ] )
+class ConfusionTable:
+	def __init__(self, name1, name2):
+		self.name1  = name1
+		self.name2 = name2
+		self.total = None
 
-	print " "*10 + '|' + "{:<10}".format(name1)+ '|' + "{:<10}".format(name2) + ' ===> Expected classes'
-	print "{:<10}".format(name1) + '|' + "{:<10}".format(one_correct) + '|' + "{:<10}".format(two_false)
-	print "{:<10}".format(name2) + '|' + "{:<10}".format(one_false) + '|' + "{:<10}".format(two_correct)
-	print "^"
-	print "predicted classes"
 
-	one_correct = round( float(one_correct)/float(train_x.shape[0])*100 , 2 )
-	two_correct = round( float(two_correct)/float(train_x.shape[0])*100 , 2 )
-	one_false = round( float(one_false)/float(train_x.shape[0])*100 , 2 )
-	two_false = round(  float(two_false)/float(train_x.shape[0])*100 , 2 )
-	print ""
+	def imprimir(self):
+		print(" "*10 + '|' + "{:<10}".format(self.name1)+ '|' + "{:<10}".format(self.name2) + ' ===> Expected classes' )
+		print("{:<10}".format(self.name1) + '|' + "{:<10}".format(self.table[0][0]) + '|' + "{:<10}".format(self.table[0][1]))
+		print("{:<10}".format(self.name2) + '|' + "{:<10}".format(self.table[1][0]) + '|' + "{:<10}".format(self.table[1][1]))
+		print("^")
+		print("predicted classes")
 
-	print " "*10 + '|' + "{:<10}".format(name1)+ '|' + "{:<10}".format(name2) + ' ===> Expected classes'
-	print "{:<10}".format(name1) + '|' + "{:<10}".format(one_correct) + '|' + "{:<10}".format(two_false)
-	print "{:<10}".format(name2) + '|' + "{:<10}".format(one_false) + '|' + "{:<10}".format(two_correct)
-	print "^"
-	print "predicted classes"
+
+	def imprimir_porcentagem(self):
+		percentage_table = (self.table /float(self.total)*100).round(2)
+		print("")
+		print(" "*10 + '|' + "{:<10}".format(self.name1)+ '|' + "{:<10}".format(self.name2) + ' ===> Expected classes')
+		print("{:<10}".format(self.name1) + '|' + "{:<10}".format(percentage_table[0][0]) + '|' + "{:<10}".format(percentage_table[0][1]))
+		print("{:<10}".format(self.name2) + '|' + "{:<10}".format(percentage_table[1][0]) + '|' + "{:<10}".format(percentage_table[1][1]))
+		print("^")
+		print("predicted classes")
+
+
+	def gerar(self, train_x, test_x, train_y, test_y, model):
+		model.fit( train_x, train_y.flatten() )
+		out = np.array([ model.predict( elem.reshape(1, -1))  for elem in test_x ]).round().astype(int)
+		self.total = test_y.shape[0]
+		correct = [ bool(_ == __) for _,__ in zip( out,test_y )]
+		one_correct =  np.sum([ _ == True and cl == 1 for _, cl in zip(correct,  test_y)]) 
+		two_correct =  np.sum([ _ == True and cl == 2 for _, cl in zip(correct,  test_y)])
+		one_false = np.sum([ _ == False and cl == 1 for _, cl in zip(correct,  test_y)])
+		two_false = np.sum([ _ == False and cl == 2 for _, cl in zip(correct,  test_y)])
+		self.table = np.array([[one_correct, one_false],[two_correct,two_false]])
+
 
--- testes.py
+++ testes.py
@@ -6,32 +6,57 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 import numpy as np
 import pandas as pd
-from tables import tables
 from xgboost import XGBClassifier
 import matplotlib.pyplot as plt
+import tables
 
-def main():
-	dataset = pd.read_csv("Archive/data_44100_1024_treino.csv").as_matrix()
-	dataset2 = pd.read_csv("Archive/data_44100_1024_teste.csv").as_matrix()
+
+'''
+Dataset should be an numpy array.
+'''
+def normalizar( dataset ):
+	dataset = dataset.astype(float)
+	for i in range( dataset.shape[1] ):
+		dataset[:,i] = dataset[:,i] / np.amax( dataset[:,i])
+	return dataset
+
+
+def gerar_curva_de_aprendizado():
+	treino = pd.read_csv("Archive/data_44100_1024_treino.csv").as_matrix()
+	teste = pd.read_csv("Archive/data_44100_1024_teste.csv").as_matrix()
 
 	mlp = MLPClassifier(hidden_layer_sizes=(60), learning_rate = 'constant', max_iter = 1000, activation = 'logistic', learning_rate_init = 0.01, shuffle = False )
 	rf = RandomForestClassifier(100, max_depth = 7)
-	dataset = ca.normalizar(dataset)
-	dataset2 = ca.normalizar(dataset2)
-	np.random.shuffle(dataset)
-	np.random.shuffle(dataset2)
+	treino = ca.normalizar(treino)
+	teste = ca.normalizar(teste)
+	np.random.shuffle(treino)
+	np.random.shuffle(teste)
 
 	plt.title("data_44100_1024_treino e dados normalizados", fontsize = 12)
-	ca.curva_aprendizagem( mlp, dataset[:600], y_start = 0, model_name = "Multilayer Perceptron", number_of_splits = 200, number_of_runs=10 )
+	ca.curva_aprendizagem( mlp, treino[:600], y_start = 0, model_name = "Multilayer Perceptron", number_of_splits = 200, number_of_runs=10 )
 	
-	dataset = np.concatenate((dataset, dataset2))
+	treino = np.concatenate((treino, teste))
 
 	plt.title("data_44100_1024_treino e data_44100_1024_teste com dados normalizados", fontsize = 12)
-	ca.curva_aprendizagem( mlp, dataset[:600], y_start = 0, model_name = "Multilayer Perceptron", number_of_splits = 200, number_of_runs=10 )
+	ca.curva_aprendizagem( mlp, treino[:600], y_start = 0, model_name = "Multilayer Perceptron", number_of_splits = 200, number_of_runs=10 )
 
 	plt.title("data_44100_1024_treino e data_44100_1024_teste com altura limitada(7)", fontsize = 12)
-	ca.curva_aprendizagem( rf, dataset, y_start = 90 ,model_name = "Random Forest", number_of_splits = 200, number_of_runs = 1 )
+	ca.curva_aprendizagem( rf, treino, y_start = 90 ,model_name = "Random Forest", number_of_splits = 200, number_of_runs = 1 )
 
 	plt.show()
 
-main()
+
+def gerar_tabela_de_confusao():
+	tabela = tables.ConfusionTable( "fruitfly", "jatai" )
+	treino = pd.read_csv("Archive/data_44100_1024_treino.csv").as_matrix()
+#	treino = normalizar( treino )
+	np.random.shuffle(treino)
+	mlp = MLPClassifier(hidden_layer_sizes=(60), learning_rate = 'constant', max_iter = 1000, activation = 'logistic', learning_rate_init = 0.01, shuffle = False )
+	teste = pd.read_csv("Archive/data_44100_1024_teste.csv").as_matrix()
+#	teste = normalizar(teste)
+	tabela.gerar( model = mlp, train_y = treino[:,0], test_y = teste[:,0], train_x = treino[:,1:], test_x = treino[:,1:])
+	tabela.imprimir()
+	tabela.imprimir_porcentagem()
+
+
+gerar_curva_de_aprendizado()
